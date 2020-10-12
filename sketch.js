07b87cfa64e18486690f14b5f912eaf2b1ca4256
Joseph Fischetti <fischettijw@gmail.com>
2020-10-12 10:06:57 -0400

    created 3 digit pie random number

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let pie;
 let index = 0;
+let rndIndex = 0;
 
 function setup() {
     createCanvas(600, 400);
@@ -7,7 +8,7 @@ function setup() {
     textSize(36);
     textAlign(CENTER, CENTER);
     pie = Array.from(getPie());
-    // frameRate(2);
+    frameRate(2);
 }
 
 function draw() {
@@ -16,6 +17,7 @@ function draw() {
     text(pie[index], width / 2, height / 4);
     text(frameRate(), width / 2, height / 3);
     text(pie.length, width / 2, height / 2);
+    text(randomPie(), width / 2, height / 1.5);
     // text(pie, width / 2, height / 1.5); // reduces frameRate ~ 7 fold
     index++;
 
@@ -23,4 +25,13 @@ function draw() {
         index = 0;
     }
 
+}
+
+function randomPie() {
+    let rndPie = (pie[rndIndex + 0] * 100) + (pie[rndIndex + 1] * 10) + (pie[rndIndex + 2] * 1);
+    rndIndex += 3;
+    if (rndIndex > (pie.length - 3)) {
+        rndIndex = 0;
+    }
+    return rndPie;
 }
