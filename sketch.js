d950fb7d890a25a2eafa24b01d864436ad841fa9
Joseph Fischetti <fischettijw@gmail.com>
2020-10-12 10:17:33 -0400

    maybe finished with pieDigits()

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 let pie;
 let index = 0;
-let rndIndex = 0;
 
 function setup() {
     createCanvas(600, 400);
     fill(255);
     textSize(36);
     textAlign(CENTER, CENTER);
-    pie = Array.from(getPie());
-    frameRate(2);
+    pie = Array.from(getPie()); // getPie() in pieDigits.js
+    // frameRate(1); // frameRate for testing pieDigits.js
 }
 
 function draw() {
@@ -17,21 +16,14 @@ function draw() {
     text(pie[index], width / 2, height / 4);
     text(frameRate(), width / 2, height / 3);
     text(pie.length, width / 2, height / 2);
-    text(randomPie(), width / 2, height / 1.5);
+    text(rndIndex, width / 2, height / 1.5); // rndIndex in pieDigits.js
+    text(randomPie(), width / 2, height / 1.2); // randomPie() in pieDigits.js
     // text(pie, width / 2, height / 1.5); // reduces frameRate ~ 7 fold
+
     index++;
 
     if (index > pie.length) {
         index = 0;
     }
 
-}
-
-function randomPie() {
-    let rndPie = (pie[rndIndex + 0] * 100) + (pie[rndIndex + 1] * 10) + (pie[rndIndex + 2] * 1);
-    rndIndex += 3;
-    if (rndIndex > (pie.length - 3)) {
-        rndIndex = 0;
-    }
-    return rndPie;
 }
